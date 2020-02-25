a42c08eda5faa43f13227fa7e997ce0d2c2e3604
Naman Gupta <namanryan@gmail.com>
2020-02-25 17:33:06 -0500

    Updated read_laser.py

--- scripts/read_laser.py
+++ scripts/read_laser.py
@@ -3,20 +3,14 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 
-def clbk_laser(msg):
+def laser_read(msg):
     # 720/5 = 144
-    regions = [ 
-      min(msg.ranges[0:143]),
-      min(msg.ranges[144:287]),
-      min(msg.ranges[288:431]),
-      min(msg.ranges[432:575]),
-      min(msg.ranges[576:713]),
-     ]
-    rospy.loginfo(regions)
+    area = [min(msg.ranges[0:143]), min(msg.ranges[144:287]), min(msg.ranges[288:431]), min(msg.ranges[432:575]), min(msg.ranges[576:713]),]
+    rospy.loginfo(area)
 
 def main():
     rospy.init_node('read_laser')
-    sub= rospy.Subscriber("/laser/scan", LaserScan, clbk_laser)
+    sub= rospy.Subscriber("/laser/scan", LaserScan, laser_read)
     rospy.spin()
 
 if __name__ == '__main__':
